cf7cab379367b82ea1566a5639238060613b72c6
wbvolkan <56910588+wbvolkan@users.noreply.github.com>
2020-12-11 21:56:44 +0300

    Update application.py

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
-		a = requet.json['text']
+		a = request.json['text']
 		return a
 	return '<h1> s.a aq </h1>'
 if __name__ == '__main__':
